9b3b56071e2f5d21d9334ac1f80b169ac1aeba8d
Archie Fox <ajfox123@gmail.com>
2021-12-11 15:06:07 +1100

    Scrape iq for financials and execs

--- test.py
+++ test.py
@@ -17,6 +17,7 @@ from pprint import pprint
 import pickle
 
 
+
 def main():
     url = 'https://www.capitaliq.com/ciqdotnet/login-sso.aspx'
     driver = webdriver.Chrome()
@@ -31,8 +32,6 @@ def main():
         
     driver.get('https://www.capitaliq.com/CIQDotNet/my/dashboard.aspx')
 
-    search_bar = driver.find_element_by_name("SearchText")
-    
     url = 'https://estateinnovation.com/101-top-australia-construction-companies-and-startups-innovating-the-industry/'
     response = requests.get(url)
     soup = bs(response.content, 'html.parser')
@@ -46,9 +45,111 @@ def main():
         name = company.xpath('.//h3[@class="has-huge-font-size"]//descendant::span//text()')[0]
         links = dict(zip(company.xpath('.//a/text()'), company.xpath('.//a/@href')))
         data["Name"] = name 
+        print(name)
         for link in links:
             data[link] = links[link]
             
+        found = find_company_page(name, driver)
+        if found:
+            print('found company')
+            iq_data = scrape_iq_data(driver)
+            data = {**data, **iq_data}
+        else:
+            print('not found')
+        company_data.append(data)
+        print('\n')
+        
+    df = pd.DataFrame(company_data)
+    df.to_csv("company_data_NEW.csv", index=False)
+        
+        
+            
+def find_company_page(name, driver):
+    search_bar = driver.find_element_by_name("SearchText")
+    search_bar.send_keys(name)
+    search_bar.send_keys(Keys.ENTER)
+    
+    if 'company' in driver.current_url:
+        # On company page
+        return True
+
+    elif 'Please see some suggested matches:' in driver.page_source:
+        # Pick from suggestion
+        company_suggestions = driver.find_elements_by_xpath('//table//*[contains(text(), "Company)")]')
+        print('going through suggestions')
         try:
-            search_bar.send_keys(name)
-            search_bar.send_keys(Keys.ENTER)
+            for i in range(min(5, len(company_suggestions))):
+                href = company_suggestions[i]
+                print('trying:    ', href.text)
+                href.click()
+                offices = driver.find_element_by_xpath('//a[contains(text(), "View All Office Addresses")]')
+                primary_office = offices.find_element_by_xpath('./../../../../following-sibling::*').text.split('\n')[0]
+                if 'Australia' in primary_office:
+                    return True
+                else:
+                    driver.back()
+                    company_suggestions = driver.find_elements_by_xpath('//table//*[contains(text(), "Company)")]')
+            print(f'not found in first few links')
+            return False
+            
+        except:
+            print('need to debug')
+            time.sleep(5)
+    else:
+        company_suggestions = driver.find_elements_by_xpath('//table[@class="cTblListBody"]//*[@href]')
+        print('going through search results')
+        try:
+            for i in range(min(5, len(company_suggestions))):
+                href = company_suggestions[i]
+                print('trying:    ', href.text)
+                href.click()
+                offices = driver.find_element_by_xpath('//a[contains(text(), "View All Office Addresses")]')
+                primary_office = offices.find_element_by_xpath('./../../../../following-sibling::*').text.split('\n')[0]
+                if 'Australia' in primary_office:
+                    return True
+                else:
+                    driver.back()
+                    company_suggestions = driver.find_elements_by_xpath('//table[@class="cTblListBody"]//*[@href]')
+            return False
+        except:
+            time.sleep(5)
+            print('need to debug')
+            return False
+
+
+
+def scrape_iq_data(driver):
+    data = {}
+    revenue = driver.find_element_by_xpath('//td//*[contains(text(), "Total Revenue")]').find_element_by_xpath('..//..//a').text
+    revenue = float(revenue.replace(",", ""))*(10**6)
+    data['Revenue'] = revenue
+    
+    ebitda = driver.find_element_by_xpath('//td//*[contains(text(), "EBITDA")]').find_element_by_xpath('..//..//a').text
+    ebitda = float(ebitda.replace(",", ""))*(10**6)
+    data['EBITDA'] = ebitda
+    
+    d = pd.read_html(driver.page_source)
+    for df in d[::-1]:
+        if 'Key Professionals View All' in df.values:
+            execs = df.rename(columns=df.iloc[1]).drop(df.index[:2]).reset_index(drop=True).iloc[:, :2]
+            execs['Title'] = execs['Title'].str.upper().str.replace(',', ' ')
+            try:
+                ceo = execs[execs.Title.apply(lambda x: any(identifier in x for identifier in ['CEO', 'CHIEF EXECUTIVE OFFICER', "CHIEF EXEC. OFFICER"]))].Name.values[0]
+                data['CEO'] = ceo
+                print(ceo)
+            except:
+                print('ceo not found')
+            try:
+                cfo = execs[execs.Title.apply(lambda x: any(identifier in x for identifier in ['CFO', 'CHIEF FINANCIAL OFFICER']))].Name.values[0]
+                data['CFO'] = cfo
+                print(cfo)
+            except:
+                print('cfo not found')
+            try:
+                cto = execs[execs.Title.apply(lambda x: any(identifier in x.replace('DIRECTOR', '') for identifier in ['CTO', 'CHIEF TECHNOLOGY OFFICER']))].Name.values[0]
+                data['CTO'] = cto
+                print(cto)
+            except:
+                print('cto not found')
+            return data
+main()
